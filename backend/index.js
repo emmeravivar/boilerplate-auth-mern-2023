945d527e0d326c616bf617054864bae33a1a7f51
emmeravivar <em.meravivar@gmail.com>
2024-02-07 19:50:03 +0100

    server

--- backend/index.js
+++ backend/index.js
@@ -23,18 +23,18 @@ connectBD()
 
 //Configurar CORS
     // WhiteList
-    const whitelist = [process.env.FRONT_END_URL, process.env.FRONT_END_URL_LOCALHOST]
-    const corsOptions = {
-        origin: (origin, callback) => {
-            console.log(origin)
-            if (whitelist.includes(origin)) {
-                callback(null, true)
-            } else {
-                callback(new Error('Not allowed by CORS'))
-            }
-        }
-    }
-app.use(cors(corsOptions))
+    // const whitelist = [process.env.FRONT_END_URL, process.env.FRONT_END_URL_LOCALHOST]
+    // const corsOptions = {
+    //     origin: (origin, callback) => {
+    //         console.log(origin)
+    //         if (whitelist.includes(origin)) {
+    //             callback(null, true)
+    //         } else {
+    //             callback(new Error('Not allowed by CORS'))
+    //         }
+    //     }
+    // }
+// app.use(cors(corsOptions))
 
 //Routing
 app.use("/api/user", usersRoutes)
